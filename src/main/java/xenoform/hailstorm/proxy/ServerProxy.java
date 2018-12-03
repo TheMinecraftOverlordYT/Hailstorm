67cf848190da037769c2b935bbb6cf6da8d24df6
TheMinecraftOverlordYT <kyurembeats2@gmail.com>
2018-12-02 21:38:54 -0500

    Snow Roller

--- src/main/java/xenoform/hailstorm/proxy/ServerProxy.java
+++ src/main/java/xenoform/hailstorm/proxy/ServerProxy.java
@@ -1,21 +1,26 @@
 package xenoform.hailstorm.proxy;
 
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.item.Item;
-import net.minecraft.util.math.RayTraceResult;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
-import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.fml.common.registry.VillagerRegistry;
+import xenoform.hailstorm.MTrades;
 
 public class ServerProxy {
 
 	public void postInit(FMLPostInitializationEvent event) {
 	}
 
-	public void init(FMLInitializationEvent event) {
+	public void init(FMLInitializationEvent event)
+    {
+        VillagerRegistry.VillagerProfession cleric = ForgeRegistries.VILLAGER_PROFESSIONS.getValue(new ResourceLocation("minecraft:cleric"));
+        if(cleric != null)
+        {
+            cleric.getCareer(1).addTrade(3, new MTrades());
+            System.out.println("addtrade");
+        }
 	}
 
 	public void preInit(FMLPreInitializationEvent e) {
